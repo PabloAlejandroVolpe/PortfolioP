531af9cf11eadc0cd1bc17aa8ae6f9f596b2955c
Pablo Volpe <pablo.volpe03999@gmail.com>
2023-10-25 00:11:36 -0300

    little changes

--- app.js
+++ app.js
@@ -21,16 +21,15 @@ window.addEventListener("mousemove", function (e) {
 
 //PRELOADER
 document.addEventListener("DOMContentLoaded", function () {
-    // Gradualmente mostrar el contenido y ocultar el preloader con el GIF
     setTimeout(function () {
         const preloader = document.querySelector(".preloader");
-        const content = document.querySelector(".content"); // Agrega esta línea para seleccionar el contenido
-        preloader.style.opacity = "0"; // Cambia la opacidad del preloader a 0
+        const content = document.querySelector(".content"); 
+        preloader.style.opacity = "0"; 
         setTimeout(function () {
-            preloader.style.display = "none"; // Oculta el preloader
-            content.style.opacity = "1"; // Cambia la opacidad del contenido a 1 (completamente visible)
-        }, 1500); // Puedes ajustar el tiempo de visualización del preloader (en milisegundos)
-    }, 1500); // Puedes ajustar el tiempo de visualización del preloader (en milisegundos)
+            preloader.style.display = "none"; 
+            content.style.opacity = "1"; 
+        }, 1300); 
+    }, 1300); 
 });
 
 
