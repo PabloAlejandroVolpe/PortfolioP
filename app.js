9489e6848afb8256c263aef80df92a7e4bc14ff7
Pablo Volpe <pablo.volpe03999@gmail.com>
2023-11-02 11:32:14 -0300

    Add carrousel in a section proyects, first changes in landing

--- app.js
+++ app.js
@@ -32,5 +32,33 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1300); 
 });
 
+ 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
