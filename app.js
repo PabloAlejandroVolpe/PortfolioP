82af86999537a6d958ae58a5bdf2fd460dcf2934
Pablo Volpe <pablo.volpe03999@gmail.com>
2023-11-03 14:39:48 -0300

    changes landing

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ document.addEventListener("DOMContentLoaded", function () {
     }, 1300); 
 });
 
- 
-var swiper = new Swiper(".slide-content", {
+ //CAROUSEL
+let swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: true,
@@ -62,3 +62,33 @@ var swiper = new Swiper(".slide-content", {
         },
     },
   });
+
+  function setup(){
+    createCanvas(windowWidth, windowHeight);
+  }
+  function draw(){
+    background(150);
+    noStroke();
+    for(let i = 0; i<14000; i++){
+      rect(random(width), random(height), 2, 2);
+    }
+  }
+
+
+  let count = 40;
+let c = 1 / count;
+function draw() {
+  ctx.strokeStyle = 'hsl(180, 70%, 50%)';
+  ctx.beginPath();
+  let time_ = Date.now() * 0.0001;
+  for (let i = 0; i < count; i++) {
+    let t_ = i * c;
+    let time = (time_ + t_) % 1;
+    let t = ease.quint.in(time, 0, 1, 1);
+    let ty = ease.quint.out(t, 0, 1, 1);
+    let x = lerp(width, 0, t);
+    let y = ty * height * 0.4;
+    line(x, y, x, height - y, false);
+  }
+  ctx.stroke();
+}
