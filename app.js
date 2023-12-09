472bca9915cbb26f980074b9b6e212ed0175f071
Pablo Volpe <pablo.volpe03999@gmail.com>
2023-12-09 00:42:07 -0300

    add contact section

--- app.js
+++ app.js
@@ -17,5 +17,27 @@ window.addEventListener("mousemove", function (e) {
         { duration: 500, fill: "forwards" }
     );
 });
+//FIN CURSOR
 
 
+//CONTACTO
+
+let copyText = document.querySelector(".copy-text");
+copyText.querySelector("button").addEventListener("click", function () {
+	let input = copyText.querySelector("input.text");
+	input.select();
+	document.execCommand("copy");
+	copyText.classList.add("active");
+	window.getSelection().removeAllRanges();
+	setTimeout(function () {
+		copyText.classList.remove("active");
+	}, 2500);
+});
+
+
+const enviarBoton = document.querySelector('.enviar');
+
+// Agregar un evento clic al botón
+enviarBoton.addEventListener('click', function() {
+  window.location.href = 'mailto:pablo.volpe03999@gmail.com';
+});
